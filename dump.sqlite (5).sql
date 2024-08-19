c6e7f5ccde482607f0110feac31d362d67014c66
LucasAmorim06 <159054719+LucasAmorim06@users.noreply.github.com>
2024-08-19 14:03:03 -0300

    Add files via upload

---
+++ dump.sqlite (5).sql
@@ -0,0 +1,23 @@
+-- TABLE
+CREATE TABLE demo (ID integer primary key, Name varchar(20), Hint text );
+CREATE TABLE produto(
+ id INTEGER PRIMARY KEY AUTOINCREMENT,
+ nome VARCHAR (100) NOT NULL,
+ descricao TEXT,
+ preco DECIMAL(10,2),
+ dataHora DATETIME DEFAULT CURRENT_TIMESTAMP
+);
+CREATE TABLE produtos(
+ id INTEGER PRIMARY KEY AUTOINCREMENT,
+ nome VARCHAR (100) NOT NULL,
+ descrição,
+ preco DECIMAL(10,2),
+ dataHora DATETIME DEFAULT CURRENT_TIMESTAMP
+);
+ 
+-- INDEX
+ 
+-- TRIGGER
+ 
+-- VIEW
+ 
